050a35b01a39dfe374984ca111b4a32f013f81ba
LuisaCarol <153710602+LuisaCarol@users.noreply.github.com>
2023-12-20 21:17:46 -0500

    Add files via upload

---
+++ Operadores aritméticos.py
@@ -0,0 +1,32 @@
+# Operadores Aritméticos en Python
+Estos operadores son esenciales en programación para realizar cálculos matemáticos en algoritmos, fórmulas matemáticas y manipulación de datos numéricos.
+Pueden ser utilizados en combinación y en diversas situaciones para realizar operaciones matemáticas avanzadas en programas escritos en Python.
+# Suma
+a = 5
+b = 3
+suma = a + b
+print(f"Suma: {a} + {b} = {suma}  # Suma dos valores")
+
+# Resta
+resta = a - b
+print(f"Resta: {a} - {b} = {resta}  # Resta el valor de la derecha del valor de la izquierda")
+
+# Multiplicación
+multiplicacion = a * b
+print(f"Multiplicación: {a} * {b} = {multiplicacion}  # Multiplica dos valores")
+
+# División
+division = a / b
+print(f"División: {a} / {b} = {division}  # Divide el valor de la izquierda por el valor de la derecha")
+
+# Cociente de la división entera
+cociente = a // b
+print(f"Cociente de la división entera: {a} // {b} = {cociente}  # Devuelve el cociente entero de la división")
+
+# Resto de la división entera
+resto = a % b
+print(f"Resto de la división entera: {a} % {b} = {resto}  # Devuelve el resto de la división")
+
+# Potencia
+potencia = a ** b
+print(f"Potencia: {a} ** {b} = {potencia}  # Eleva el valor de la izquierda a la potencia del valor de la derecha")
